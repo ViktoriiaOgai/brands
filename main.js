7f7121620862e9e54a53fb5da2365943e7edc47e
Viktoriia <ogay_viktory@gmail.com>
2025-08-24 23:20:17 +0900

    Все три расширения

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // --- Swiper мобильный ---
+  // --- Swiper для мобильной версии ---
   let swiper;
   function initSwiper() {
     if (window.innerWidth < 768) {
@@ -8,10 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
           slidesPerView: 1.2,
           spaceBetween: 16,
           loop: true,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          }
+          pagination: { el: '.swiper-pagination', clickable: true },
         });
       } else {
         swiper.update();
@@ -26,14 +23,23 @@ document.addEventListener('DOMContentLoaded', () => {
   initSwiper();
   window.addEventListener('resize', initSwiper);
 
-  // --- Кнопка Показать все ---
-  const toggleBtn = document.querySelector('.brands__toggle');
-  const hiddenCards = document.querySelectorAll('.brands__listt .brand-cardt:nth-child(n+7)');
-  const linkText = toggleBtn.querySelector('.link-text');
-
-  toggleBtn.addEventListener('click', () => {
+  // --- Кнопка Показать все для планшета ---
+  const toggleTab = document.querySelector('.brands__toggle');
+  toggleTab.addEventListener('click', () => {
+    const hiddenCards = document.querySelectorAll('.brands__listt .brand-cardt:nth-child(n+7)');
     hiddenCards.forEach(card => card.classList.toggle('show'));
-    toggleBtn.classList.toggle('open');
-    linkText.textContent = toggleBtn.classList.contains('open') ? 'Свернуть' : 'Показать все';
+    toggleTab.classList.toggle('open');
+    const linkText = toggleTab.querySelector('.link-text');
+    linkText.textContent = toggleTab.classList.contains('open') ? 'Свернуть' : 'Показать все';
+  });
+
+  // --- Кнопка Показать все для десктопа ---
+  const toggleTabl = document.querySelector('.brands__tabl__toggle');
+  toggleTabl.addEventListener('click', () => {
+    const hiddenCards = document.querySelectorAll('.brands__listtabl .brand-cardttabl:nth-child(n+9)');
+    hiddenCards.forEach(card => card.classList.toggle('show'));
+    toggleTabl.classList.toggle('open');
+    const linkText = toggleTabl.querySelector('.link-text');
+    linkText.textContent = toggleTabl.classList.contains('open') ? 'Свернуть' : 'Показать все';
   });
 });
